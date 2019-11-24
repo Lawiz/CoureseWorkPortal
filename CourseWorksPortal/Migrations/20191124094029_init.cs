9ce0a779269a98bd0d420b52afa7d5671729132a
Petr <petrsobolev1432@gmail.com>
2019-11-24 12:47:41 +0300

    feateature: -initial project, add db, add models

---
+++ CourseWorksPortal/Migrations/20191124094029_init.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CourseWorksPortal.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Users",
+                nullable: false,
+                oldClrType: typeof(int))
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "FavoriteWorks",
+                nullable: false,
+                oldClrType: typeof(int))
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "CourseWorks",
+                nullable: false,
+                oldClrType: typeof(int))
+                .Annotation("SqlServer:Identity", "1, 1");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Users",
+                nullable: false,
+                oldClrType: typeof(int))
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "FavoriteWorks",
+                nullable: false,
+                oldClrType: typeof(int))
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "CourseWorks",
+                nullable: false,
+                oldClrType: typeof(int))
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+        }
+    }
+}
---
+++ CourseWorksPortal/Models/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourseWorksPortal.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<CourseWork> CourseWorks { get; set; }
+        public DbSet<FavoriteWork> FavoriteWorks { get; set; }
+       
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            //builder.Entity<User>().HasAlternateKey(u => u.Username);
+        }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
